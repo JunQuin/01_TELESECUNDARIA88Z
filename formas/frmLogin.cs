01258bec34a66ecf3f55ad33181555ed7923cec4
JunQuin <jun_quin@outlook.com>
2020-10-28 17:25:56 -0700

    LOGIN FUNCIONAL
    -Encriptado con MD5
    -Cargar de Imagenes de usuario
    -Redirección a menu

---
+++ formas/frmLogin.cs
@@ -0,0 +1,193 @@
+﻿using _01_TELESECUNDARIA88Z.clases;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_TELESECUNDARIA88Z.formas
+{
+    public partial class frmLogin : Form
+    {
+        string sNivel;
+        string sUserName;
+        clases.conexion objConexion;
+        MySqlConnection mySqlConex;
+        public frmLogin()
+        {
+            InitializeComponent();
+            txtUser.Focus();
+            lblCargando.Visible = false;
+            txtPassword.Enabled = false;
+            btningresar.Enabled = false;
+        }
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+            formas.frmMenu frmMenu = new frmMenu();
+            frmMenu.Text = "TELESECUNDARIA#88Z ver. 0.1.23.1" + "       " + "USUARIO: " + sUserName + "       " + "NIVEL: " + txtNivel.Text;
+            MessageBox.Show("Bienvenido al sistema: " + sUserName + " " + "Nivel: " + txtNivel.Text);
+            frmMenu.ShowDialog();
+            this.Close();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void txtUser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                if (string.IsNullOrEmpty(txtUser.Text.Trim()))
+                {
+                    MessageBox.Show("ESTE CAAMPO NO PUEDE SER NULO!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtUser.Clear();
+                    txtUser.Focus();
+                }
+                else
+                {
+                    lblCargando.Visible = true;
+                    objConexion = new clases.conexion();
+                    mySqlConex = new MySqlConnection(objConexion.conn());
+                    mySqlConex.Open();
+                    string mySqlQuery = "select * from usuarios where us_login = @us_login;";
+                    MySqlCommand mySqlCommand = new MySqlCommand(mySqlQuery, mySqlConex);
+                    mySqlCommand.Parameters.Clear();
+                    mySqlCommand.Parameters.AddWithValue("@us_login", txtUser.Text);
+                    MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+                    if (mySqlDataReader.Read())
+                    {
+                        sUserName = mySqlDataReader["us_nombre"].ToString();
+                        sNivel = mySqlDataReader["us_nivel"].ToString();
+                        if (sNivel == "1")
+                        {
+                            txtNivel.Text = "Administrador";
+                        }
+                        else if (sNivel == "2")
+                        {
+                            txtNivel.Text = "Operador";
+                        }
+                        txtPassword.Enabled = true;
+                        txtPassword.Focus();
+                        txtUser.Enabled = false;
+
+                        try
+                        {
+                            MemoryStream memoryStream = new MemoryStream((byte[])mySqlDataReader["us_imagen"]);
+                            Bitmap bitmap = new Bitmap(memoryStream);
+                            pboxUser.Image = bitmap;
+
+                        }
+                        catch
+                        {
+                            var bitmap = new Bitmap(_01_TELESECUNDARIA88Z.Properties.Resources.usuario);
+                            pboxUser.Image = bitmap;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario Invalido!","Usuario No Encontrado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        txtUser.Clear();
+                        txtUser.Focus();
+                    }
+                    mySqlConex.Close();
+                    lblCargando.Visible = false;
+                }
+            }
+            if (e.KeyChar == Convert.ToChar(Keys.Escape))
+            {
+                txtUser.Clear();
+            }
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Escape))
+            {
+                txtPassword.Clear();
+                txtUser.Clear();
+                txtNivel.Clear();
+                txtUser.Enabled = true;
+                txtPassword.Enabled = false;
+                var bitmap = new Bitmap(_01_TELESECUNDARIA88Z.Properties.Resources.usuario);
+                pboxUser.Image = bitmap;
+                txtUser.Focus();
+                lblCargando.Visible = false;
+            }
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {                
+                if (string.IsNullOrEmpty(txtPassword.Text.Trim()))
+                {
+                    MessageBox.Show("ESTE CAMPO NO PUEDE SER NULO!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtPassword.Clear();
+                    txtPassword.Focus();
+                }
+                var source = txtPassword.Text;             
+                using (var md5Hash = MD5.Create())
+                {
+                    // Byte array representation of source string
+                    var sourceBytes = Encoding.UTF8.GetBytes(source);
+
+                    // Generate hash value(Byte Array) for input data
+                    var hashBytes = md5Hash.ComputeHash(sourceBytes);
+
+                    // Convert hash byte array to string
+                    var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+
+                    // Output the MD5 hash
+                    // Console.WriteLine("The MD5 hash of " + source + " is: " + hash);
+                    source = hash;
+                }
+                objConexion = new clases.conexion();
+                mySqlConex = new MySqlConnection(objConexion.conn());
+                mySqlConex.Open();
+                string mySqlQuery = "select * from usuarios where us_login = @us_login and us_password = @us_password;";
+                MySqlCommand mySqlCommand = new MySqlCommand(mySqlQuery, mySqlConex);
+                mySqlCommand.Parameters.Clear();
+                mySqlCommand.Parameters.AddWithValue("@us_login", txtUser.Text);
+                mySqlCommand.Parameters.AddWithValue("@us_password", source);
+                MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+                if (mySqlDataReader.Read())
+                {
+                    txtPassword.Enabled = false;
+                    btningresar.Enabled = true;
+                    btningresar.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Contraseña Incorrecta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                mySqlConex.Close();
+            }
+        }
+
+        private void btningresar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == Convert.ToChar(Keys.Escape))
+            {
+                txtNivel.Clear();
+                txtPassword.Clear();
+                txtUser.Clear();
+                txtPassword.Enabled = false;
+                btningresar.Enabled = false;
+                txtUser.Enabled = true;
+                txtUser.Focus();
+                lblCargando.Visible = false;
+                var bitmap = new Bitmap(_01_TELESECUNDARIA88Z.Properties.Resources.usuario);
+                pboxUser.Image = bitmap;
+            }
+        }
+    }
+}
---
+++ formas/frmMenu.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _01_TELESECUNDARIA88Z.formas
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mENUCHIDORIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(30, 30);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mENUCHIDORIToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(985, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mENUCHIDORIToolStripMenuItem
+            // 
+            this.mENUCHIDORIToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mENUCHIDORIToolStripMenuItem.Name = "mENUCHIDORIToolStripMenuItem";
+            this.mENUCHIDORIToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
+            this.mENUCHIDORIToolStripMenuItem.Text = "MENU CHIDORI";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.BackgroundImage = global::_01_TELESECUNDARIA88Z.Properties.Resources.Exit_png_32x32;
+            this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSalir.Location = new System.Drawing.Point(893, 33);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(92, 43);
+            this.btnSalir.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.btnSalir, "SALIR");
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mENUCHIDORIToolStripMenuItem;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
